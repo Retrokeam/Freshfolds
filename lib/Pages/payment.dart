ac125ffcda744af4e1004f0e0dd06c82ef248f10
Retrokeam <jrdnsretro@gmail.com>
2023-04-02 14:45:44 +0300

    classes

--- lib/Pages/payment.dart
+++ lib/Pages/payment.dart
@@ -10,6 +10,7 @@ class Pay extends StatefulWidget {
 class _PayState extends State<Pay> {
 
   TextEditingController cardNumberController = TextEditingController();
+  TextEditingController cardMonthController = TextEditingController();
 
 
   @override
@@ -22,39 +23,87 @@ class _PayState extends State<Pay> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text("Card Form",
+              const Text("Card Form",
                 style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),
               ),
-              SizedBox(height: 20,),
+              //Spacer(),
+              const SizedBox(height: 20,),
               Form(
                 child: Column(
                   children: [
-                    TextFormField(
-                      controller: cardNumberController,
-                      keyboardType:TextInputType.number,
-                      inputFormatters: [
-                        FilteringTextInputFormatter.digitsOnly,
-                        LengthLimitingTextInputFormatter(16),
-                        CardNumberInputFormatter(),
-                      ],
-                      decoration: InputDecoration(hintText: "Card Number",
-                      prefixIcon: Icon(Icons.credit_card)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: cardNumberController,
+                        keyboardType:TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                          LengthLimitingTextInputFormatter(16),
+                          CardNumberInputFormatter(),
+                        ],
+                        decoration: const InputDecoration(hintText: "Card Number",
+                        prefixIcon: Icon(Icons.credit_card)
+                        ),
                       ),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: TextFormField(
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                           hintText: "Full Name",
-                          prefixIcon: Icon(Icons.account_circle_rounded)
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Icon(Icons.account_circle_rounded),
+                          )
+
                         ),
                       ),
                     ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(4),
+                            ],
+                            decoration: InputDecoration(
+                              hintText: "CVV",
+                              prefixIcon: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 10),
+                                child: Icon(Icons.card_membership_outlined),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5,),
+                        Expanded(
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(4),
+                              CardMonthInputFormatter(),
+                            ],
+                            decoration: InputDecoration(
+                              hintText: "MM/YY",
+                              prefixIcon: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 10),
+                                child: Icon(Icons.calendar_month_outlined),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),),
-
-              ElevatedButton(onPressed: (){},
-                  child: Text("Pay")
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(onPressed: (){},
+                    child: const Text("Pay")
+                ),
               ),
             ],
           ),
@@ -88,3 +137,31 @@ class CardNumberInputFormatter extends TextInputFormatter{
     );
   }
 }
+class CardMonthInputFormatter extends TextInputFormatter{
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    var newText = newValue.text;
+
+    if (newValue.selection.baseOffset == 0){
+      return newValue;
+    }
+
+    // TODO: implement formatEditUpdate
+    // throw UnimplementedError();
+    var buffer = StringBuffer();
+    for ( int i = 0; i < newText.length; i++){
+      buffer.write(newText[i]);
+      var nonZeroIndex = i + 1;
+      if (nonZeroIndex % 2 == 0 && nonZeroIndex != newText.length){
+        buffer.write("/");
+      }
+    }
+    var string = buffer.toString();
+    return newValue.copyWith(
+      text: string,
+      selection: TextSelection.collapsed(offset: string.length),
+    );
+  }
+}
+
